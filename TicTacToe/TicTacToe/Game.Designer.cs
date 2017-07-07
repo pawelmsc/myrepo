9ccc8eab78ac0de8b8bda91113ebb270837e858b
Paweł Maniara <pawelmsc@users.noreply.github.com>
2017-07-07 08:37:26 +0200

    Start 20170707

--- TicTacToe/TicTacToe/Game.Designer.cs
+++ TicTacToe/TicTacToe/Game.Designer.cs
@@ -41,8 +41,22 @@
             this.rect00 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
             this.pbx00 = new System.Windows.Forms.PictureBox();
             this.pbx10 = new System.Windows.Forms.PictureBox();
+            this.pbx20 = new System.Windows.Forms.PictureBox();
+            this.pbx21 = new System.Windows.Forms.PictureBox();
+            this.pbx11 = new System.Windows.Forms.PictureBox();
+            this.pbx01 = new System.Windows.Forms.PictureBox();
+            this.pbx22 = new System.Windows.Forms.PictureBox();
+            this.pbx12 = new System.Windows.Forms.PictureBox();
+            this.pbx02 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbx00)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx02)).BeginInit();
             this.SuspendLayout();
             // 
             // shapeContainer1
@@ -66,19 +80,19 @@
             // 
             // rect22
             // 
-            this.rect22.Location = new System.Drawing.Point(177, 156);
+            this.rect22.Location = new System.Drawing.Point(178, 157);
             this.rect22.Name = "rect22";
             this.rect22.Size = new System.Drawing.Size(70, 70);
             // 
             // rect21
             // 
-            this.rect21.Location = new System.Drawing.Point(107, 156);
+            this.rect21.Location = new System.Drawing.Point(107, 157);
             this.rect21.Name = "rect21";
-            this.rect21.Size = new System.Drawing.Size(70, 70);
+            this.rect21.Size = new System.Drawing.Size(71, 70);
             // 
             // rect20
             // 
-            this.rect20.Location = new System.Drawing.Point(37, 156);
+            this.rect20.Location = new System.Drawing.Point(37, 157);
             this.rect20.Name = "rect20";
             this.rect20.Size = new System.Drawing.Size(70, 70);
             // 
@@ -86,23 +100,23 @@
             // 
             this.rect10.Location = new System.Drawing.Point(37, 86);
             this.rect10.Name = "rect10";
-            this.rect10.Size = new System.Drawing.Size(70, 70);
+            this.rect10.Size = new System.Drawing.Size(70, 71);
             // 
             // rect11
             // 
             this.rect11.Location = new System.Drawing.Point(107, 86);
             this.rect11.Name = "rect11";
-            this.rect11.Size = new System.Drawing.Size(70, 70);
+            this.rect11.Size = new System.Drawing.Size(71, 71);
             // 
             // rect12
             // 
-            this.rect12.Location = new System.Drawing.Point(177, 86);
+            this.rect12.Location = new System.Drawing.Point(178, 86);
             this.rect12.Name = "rect12";
-            this.rect12.Size = new System.Drawing.Size(70, 70);
+            this.rect12.Size = new System.Drawing.Size(70, 71);
             // 
             // rect02
             // 
-            this.rect02.Location = new System.Drawing.Point(177, 16);
+            this.rect02.Location = new System.Drawing.Point(178, 16);
             this.rect02.Name = "rect02";
             this.rect02.Size = new System.Drawing.Size(70, 70);
             // 
@@ -110,7 +124,7 @@
             // 
             this.rect01.Location = new System.Drawing.Point(107, 16);
             this.rect01.Name = "rect01";
-            this.rect01.Size = new System.Drawing.Size(70, 70);
+            this.rect01.Size = new System.Drawing.Size(71, 70);
             // 
             // rect00
             // 
@@ -136,11 +150,82 @@
             this.pbx10.TabStop = false;
             this.pbx10.Click += new System.EventHandler(this.pbx10_Click);
             // 
+            // pbx20
+            // 
+            this.pbx20.Location = new System.Drawing.Point(37, 158);
+            this.pbx20.Name = "pbx20";
+            this.pbx20.Size = new System.Drawing.Size(70, 70);
+            this.pbx20.TabIndex = 3;
+            this.pbx20.TabStop = false;
+            this.pbx20.Click += new System.EventHandler(this.pbx20_Click);
+            // 
+            // pbx21
+            // 
+            this.pbx21.Location = new System.Drawing.Point(108, 158);
+            this.pbx21.Name = "pbx21";
+            this.pbx21.Size = new System.Drawing.Size(70, 70);
+            this.pbx21.TabIndex = 6;
+            this.pbx21.TabStop = false;
+            this.pbx21.Click += new System.EventHandler(this.pbx21_Click);
+            // 
+            // pbx11
+            // 
+            this.pbx11.Location = new System.Drawing.Point(108, 87);
+            this.pbx11.Name = "pbx11";
+            this.pbx11.Size = new System.Drawing.Size(70, 70);
+            this.pbx11.TabIndex = 5;
+            this.pbx11.TabStop = false;
+            this.pbx11.Click += new System.EventHandler(this.pbx11_Click);
+            // 
+            // pbx01
+            // 
+            this.pbx01.Location = new System.Drawing.Point(108, 16);
+            this.pbx01.Name = "pbx01";
+            this.pbx01.Size = new System.Drawing.Size(70, 70);
+            this.pbx01.TabIndex = 4;
+            this.pbx01.TabStop = false;
+            this.pbx01.Click += new System.EventHandler(this.pbx01_Click);
+            // 
+            // pbx22
+            // 
+            this.pbx22.Location = new System.Drawing.Point(179, 158);
+            this.pbx22.Name = "pbx22";
+            this.pbx22.Size = new System.Drawing.Size(70, 70);
+            this.pbx22.TabIndex = 9;
+            this.pbx22.TabStop = false;
+            this.pbx22.Click += new System.EventHandler(this.pbx22_Click);
+            // 
+            // pbx12
+            // 
+            this.pbx12.Location = new System.Drawing.Point(179, 87);
+            this.pbx12.Name = "pbx12";
+            this.pbx12.Size = new System.Drawing.Size(70, 70);
+            this.pbx12.TabIndex = 8;
+            this.pbx12.TabStop = false;
+            this.pbx12.Click += new System.EventHandler(this.pbx12_Click);
+            // 
+            // pbx02
+            // 
+            this.pbx02.Location = new System.Drawing.Point(179, 16);
+            this.pbx02.Name = "pbx02";
+            this.pbx02.Size = new System.Drawing.Size(70, 70);
+            this.pbx02.TabIndex = 7;
+            this.pbx02.TabStop = false;
+            this.pbx02.Click += new System.EventHandler(this.pbx02_Click);
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pbx22);
+            this.Controls.Add(this.pbx12);
+            this.Controls.Add(this.pbx02);
+            this.Controls.Add(this.pbx21);
+            this.Controls.Add(this.pbx11);
+            this.Controls.Add(this.pbx01);
+            this.Controls.Add(this.pbx20);
             this.Controls.Add(this.pbx10);
             this.Controls.Add(this.pbx00);
             this.Controls.Add(this.shapeContainer1);
@@ -149,6 +234,13 @@
             this.Text = "Tic-Tac-Toe";
             ((System.ComponentModel.ISupportInitialize)(this.pbx00)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx02)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -167,6 +259,13 @@
         private Microsoft.VisualBasic.PowerPacks.RectangleShape rect00;
         private System.Windows.Forms.PictureBox pbx00;
         private System.Windows.Forms.PictureBox pbx10;
+        private System.Windows.Forms.PictureBox pbx20;
+        private System.Windows.Forms.PictureBox pbx21;
+        private System.Windows.Forms.PictureBox pbx11;
+        private System.Windows.Forms.PictureBox pbx01;
+        private System.Windows.Forms.PictureBox pbx22;
+        private System.Windows.Forms.PictureBox pbx12;
+        private System.Windows.Forms.PictureBox pbx02;
     }
 }
 
--- TicTacToe/TicTacToe/Game.cs
+++ TicTacToe/TicTacToe/Game.cs
@@ -26,5 +26,40 @@ namespace TicTacToe
         {
             pbx10.Image = Properties.Resources.cross;
         }
+
+        private void pbx20_Click(object sender, EventArgs e)
+        {
+            pbx20.Image = Properties.Resources.circle;
+        }
+
+        private void pbx01_Click(object sender, EventArgs e)
+        {
+            pbx01.Image = Properties.Resources.cross;
+        }
+
+        private void pbx11_Click(object sender, EventArgs e)
+        {
+            pbx11.Image = Properties.Resources.circle;
+        }
+
+        private void pbx21_Click(object sender, EventArgs e)
+        {
+            pbx21.Image = Properties.Resources.cross;
+        }
+
+        private void pbx02_Click(object sender, EventArgs e)
+        {
+            pbx02.Image = Properties.Resources.circle;
+        }
+
+        private void pbx12_Click(object sender, EventArgs e)
+        {
+            pbx12.Image = Properties.Resources.cross;
+        }
+
+        private void pbx22_Click(object sender, EventArgs e)
+        {
+            pbx22.Image = Properties.Resources.circle;
+        }
     }
 }
